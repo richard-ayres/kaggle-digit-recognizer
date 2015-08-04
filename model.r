c14b4f5050dc8d08e57416ca518a6094b8d553a4
Richard Ayres <richard@bitspear.co.uk>
2015-08-04 13:13:15 +0100

    munge into list and scale, etc. then back into data frame

--- model.r
+++ model.r
@@ -29,82 +29,132 @@ get.list <- function(df = train.data) {
     return(l)
 }
 
-featureEngineering <- function (df, take.log = TRUE, scale = TRUE) {
+
+reshapeData <- function (df) {
+    # Return a list containing a vector of labels
+    figures =  alply(df, .margins = 1, function (row) { return(unlist(row[1, seq(2, NCOL(row))])) })
     
-    if (every > 1) {
-        # cut cells down
-        row.cells <- seq(1, all.rows, by = every)
-        col.cells <- seq(1, all.cols, by = every)
-        
-        columns <- c()
-        foreach(rc = row.cells) %do% {
-            columns <- c(columns, c(col.cells + (((rc[1]-1) * all.cols))))
-        }
-            
-        # cells in a block
-        block.size <- every*every
-        my.cells <- c()
-        foreach (y=0:(every-1)) %do%{ 
-            my.cells <- c(my.cells, (1:every) + y*all.cols)
-        }
-        my.cells <- my.cells-1
-                
-        df <- do.call(rbind, alply(df, .margins = 1, function (row) {
-            
-            foreach(cell = (columns+1)) %do% {
-                avg <- sum(row[1, my.cells+cell]) / block.size
-                row[1, cell] <- avg
-            }
-            
-            cell.data <- as.list(row[1, columns+1])
-            
-            return(data.frame(label = row$label, cell.data))
-        }))
+    if (!is.null(df$label)) {
+        return(list(labels = df$label, figures = figures))
+    } else {
+        return(list(figures = figures))
+    }
+}
+
+cutFigure <- function (figure, .every = every) {
+    # "figure" is a vector of pixel values
+    if (.every <= 1) {
+        return(figure)
     }
     
-    figure.columns <- seq(2, NCOL(df))
+    # cut cells down
+    row.cells <- seq(1, all.rows, by = .every)
+    col.cells <- seq(1, all.cols, by = .every)
     
-    # Log and scale the figures
-    df <- adply(df, .margins=1, function(row) {
-        # Take the log(1+x)
-        if (take.log) {
-            row[1, figure.columns] <- log1p(row[1, figure.columns])
-        }
+    # "keep.columns" is the columns we will keep
+    keep.columns <- c()
+    foreach(rc = row.cells) %do% {
+        keep.columns <- c(keep.columns, c(col.cells + (((rc[1]-1) * all.cols))))
+    }
         
-        if (scale) {
-            # All values
-            values <- unlist(row[1, figure.columns])
-            location <- 0 #mean(values)
-            range <- sd(values)
+    # cells in a block
+    block.size <- .every * .every
+    my.cells <- c()
+    foreach (y=0:(.every-1)) %do% { 
+        my.cells <- c(my.cells, (1:.every) + y*all.cols)
+    }
+    my.cells <- my.cells-1
             
-            row[1, figure.columns] <- (row[1, figure.columns] - location) / range
-        }
-        
-        return(row)
-    })
+    # Change the value in each interesting cell to the mean of 
+    # itself and its neighbours that are to be removed
+    foreach(cell = keep.columns) %do% {
+        avg <- sum( figure[my.cells + cell] ) / block.size
+        figure[cell] <- avg
+    }
+    
+    figure <- figure[keep.columns]
+    figure[is.na(figure)] <- 0
+    
+    return(figure)
+}
+
+featureScale <- function (figure) {
+    # Take the log(1+x)
+    figure <- log1p(figure)
+    
+    # All values
+    location <- 0 #mean(figure)
+    range <- sd(figure)
+    
+    figure <- (figure - location) / range
+    
+    return(figure)
+}
+
+backToDataFrame <- function (l) {
+    figures <- as.numeric(unlist(l$figures))
+    dim(figures) <- c(NROW(l$figures[[1]]), NROW(l$figures))
+    figures <- t(figures)
+    
+    if (is.null(l$labels)) {
+        df <- as.data.frame(figures)
+        names(df) <- paste0("pixel", seq(0, NCOL(df)-1))
+    } else {
+        labels <- as.numeric(l$labels)
+        df <- as.data.frame(cbind(labels, figures))
+        names(df) <- c("label", paste0("pixel", seq(0, NCOL(df)-2)))
+        df$label <- as.factor(df$label)
+    }
     
     return(df)
 }
 
+featureEngineering <- function (df, do.scale = TRUE) {
+    dl <- reshapeData(df)
+    dl$figures <- lapply(dl$figures, cutFigure)
+    if (do.scale) {
+        dl$figures <- lapply(dl$figures, featureScale)
+    }
+    df <- backToDataFrame(dl)
+    return(df)
+}
+
 if (!exists("source.data")) {
-    max.rows <- 2000
+    max.rows <- 10000
+    
     gunzip("train.csv.gz", overwrite=TRUE, remove = FALSE)
-    source.data <- read.csv("train.csv",
-                            header = TRUE,
-                            nrows=max.rows)
-    source.data$label <- as.factor(source.data$label)
+    
+    colClasses <- c("character", rep("double", all.cols*all.rows))
+    source.data <- (read.csv("train.csv", 
+                             header = TRUE, 
+                             colClasses=colClasses, 
+                             col.names = c("label", paste0("pixel", seq(0, (all.rows*all.cols)-1))),
+                             nrows=min(max.rows, 500)))
+    while(NROW(source.data) < max.rows) {
+        new.set <- (read.csv("train.csv", header = FALSE, colClasses=colClasses,
+                                       skip=1+NROW(source.data), 
+                                       nrows=500,
+                                       col.names=names(source.data)))
+        source.data <- rbind(source.data, new.set)
+    }
     unlink("train.csv")
+    rm(new.set)
+    
 }
 train.data <- source.data
 
+# Takes 10 minutes to massage 10,000 figures into 7x7
 print("Massaging data...")
 start.time <- Sys.time()
-train.data <- featureEngineering(train.data, take.log = TRUE, scale = TRUE)
+train.data <- featureEngineering(train.data)
 print(Sys.time() - start.time)
 
 partition <- createDataPartition(train.data[, "label"], p = 0.8, list = FALSE)
 train.batch <- train.data[partition,]
 test.batch <- train.data[-partition,]
+
+print("Is now a convenient time to save the image?")
+stop()  # Convenient time to save image?
 
 print("Training neurons...")
 start.time <- Sys.time()
@@ -112,7 +162,8 @@ m <- nnet(formula = label ~ .,
           data = train.batch,
           MaxNWts = 100000,
           maxit = 1000,
-          size = floor(rows*cols/2)) #floor(1.5*rows*cols/rows))
+          size = 100)
+#          size = min(50, floor(rows*cols/2) * max(1, NROW(train.batch)/4000))) #floor(1.5*rows*cols/rows))
 print(Sys.time() - start.time)
 
 test.batch$prediction <- predict(object = m, newdata = test.batch, type="class")
@@ -133,19 +184,24 @@ if (ratio > bestRatio) {
     printf("Best yet: %.3f\n",  ratio)
     
     # Now run our model against the competition data
-    if (!exists("competition.data")) {
+    if (!exists("test.data")) {
         gunzip("test.csv.gz", overwrite=TRUE, remove = FALSE)
-        competition.data <- read.csv("test.csv", header = TRUE)
+        test.data <- read.csv("test.csv", header = TRUE)
         unlink("test.csv")
+        
+        start.time <- Sys.time()
+        print(start.time)
+        test.data <- featureEngineering(test.data)
+        print(Sys.time() - start.time)
     }
     
-    output <- data.frame(ImageId=1:nrow(competition.data))
-    output$Label <- predict(object = m, newdata = competition.data, type = "class")
+    test.data$Label <- predict(object = m, newdata = test.data, type = "class")
+    test.data$ImageId <- 1:nrow(test.data)
     
     suffix <- paste(sep="-", format(Sys.time(), "%Y%m%d-%H%M"), round(ratio, 3))
     fname <- paste0("output/submission-", suffix, ".csv")
     
-    write.table(output, file=fname, sep = ",", quote = FALSE, row.names = FALSE, col.names = TRUE)
+    write.table(test.data[,c("ImageId", "Label")], file=fname, sep = ",", quote = FALSE, row.names = FALSE, col.names = TRUE)
     gzip(fname, overwrite = TRUE)
     
     # Commit to github repository
@@ -153,7 +209,7 @@ if (ratio > bestRatio) {
     system(paste(git, "add", "-u"))
     system(paste(git, "commit", "--allow-empty", "-m", paste0("'submission [ratio=", round(ratio, 3), "]'")))
     system(paste(git, "tag", paste0("submission-", round(ratio,3))))
-    system(paste(git, "push"))
+#     system(paste(git, "push"))
     
     bestRatio <- ratio
     save(bestRatio, file="bestRatio.RData")
